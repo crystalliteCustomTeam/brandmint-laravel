d810222ee8ab672f21d2c9f4cb833e92338889d2
Murtaza Zaidi <murtaza.zaidi@crystallite.com>
2024-06-26 23:57:21 +0500

    create packages

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,12 @@ import CTA from "./CTA"
 import Header from "./Header";
 import Hero from "./Hero";
 import AutoScrollSlider from "./AutoScrollSlider";
-import CallAway from "./CallAway" 
+import CallAway from "./CallAway"
 import Card from "./Card";
 import MarqueeBlock from "./MarqueeBlock"
+import Package from "./Packages/Package"
 import { ButtonVariants } from "./ui/Button"
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/Tabs"
 
-export {Header, Hero, Awards, CTA, ButtonVariants,MarqueeBlock, Tabs, TabsContent, TabsList, TabsTrigger, AutoScrollSlider, CallAway, Card};
+export { Header, Hero, Awards, CTA, ButtonVariants, MarqueeBlock, Tabs, TabsContent, TabsList, TabsTrigger, AutoScrollSlider, CallAway, Card, Package };
