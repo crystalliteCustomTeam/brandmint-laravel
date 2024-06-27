a2d2a1a3b107eb24c62e9c81b142d940b8c77cc4
devsahal-crystallite <143542001+devsahal-crystallite@users.noreply.github.com>
2024-06-27 23:44:38 +0500

    social media marketing 1 section add

--- src/app/social-media-marketing/page.jsx
+++ src/app/social-media-marketing/page.jsx
@@ -1,30 +1,47 @@
 // Components
-import { Awards, Hero, WhyChoose } from "@/components"
+import {Awards, Hero, WhyChoose} from "@/components";
 // Media
-import heroImg from 'media/socialMediaHero.png'
+import heroImg from "media/socialMediaHero.png";
+
+// Why Choose Section Data
+const chooseData = [
+  ["/social-media-marketing/speech-bubble.png", "Clear <br/> Communication", "We build relationships based on integrity and trust, deliver on our promises, and always maintain clear communication."],
+  [
+    "/social-media-marketing/customer-centric-approch.png",
+    "Customer-Centric <br/> Approch",
+    "We prioritize our client's success, understand your unique needs, and deliver tailored solutions that drive your",
+  ],
+  ["/social-media-marketing/great-idea.png", "Innovation and <br/> Excellence", "We consistently deliver innovative, highquality solutions as a team on the cutting edge of technology trends."],
+];
 
 export default function Page() {
-    return (
-        <main>
-            <div className="bg-black">
-                <Hero
-                    subTitle="America's Leading Social Media Marketing Company"
-                    title="Your Business Driven Our Social Media Marketing"
-                    desc="Jumpto1 Social Media Marketing means more traffic, more calls and more sales! We offer data-driven strategies and surprising bottom-line results to fuel your business growth. Our SMM experts pen such stories that best narrate your BRAND. Let us create the hype for your brand , as we want you to get busy cracking lucrative deals and orders!"
-                    img={heroImg}
-                    theme="dark"
-                    imgCss="absolute bottom-0 -z-10 right-0 max-w-[60%] xl:max-w-full opacity-20 lg:opacity-100"
-                    spaceBottom="pb-[150px] sm:pb-[200px] xl:pb-[250px]"
-                    marqueeCss="relative top-[-70px] z-10"
-                    titleCss="xs:text-[25px] text-[35px] xl:text-[45px] 2xl:text-[50px]"
-                />
-            </div>
-            <Awards />
-            <WhyChoose 
-            subTitle="Why Choose Us"
-            title="Because We HelpYour Business Get <br class='lg:block hidden'/> Bigger Every Passing Day!"
-            desc="We know, and you know too, that business survival is so challenging in the internet-focused world. And when 90% of U.S. firms use social media, it becomes even more challenging to survive and keep moving. However, with a sound digital marketing agency that doesn't use cookie-cutter marketing, brands can enjoy impressive growth and sales. But the question arises, where to find such an authentic marketing agency?"
-            />
-        </main>
-    )
+  return (
+    <main>
+      <div className="bg-black">
+        <Hero
+          subTitle="America's Leading Social Media Marketing Company"
+          title="Your Business Driven Our Social Media Marketing"
+          desc="Jumpto1 Social Media Marketing means more traffic, more calls and more sales! We offer data-driven strategies and surprising bottom-line results to fuel your business growth. Our SMM experts pen such stories that best narrate your BRAND. Let us create the hype for your brand , as we want you to get busy cracking lucrative deals and orders!"
+          img={heroImg}
+          theme="dark"
+          imgCss="absolute bottom-0 -z-10 right-0 max-w-[60%] xl:max-w-full opacity-20 lg:opacity-100"
+          spaceBottom="pb-[150px] sm:pb-[200px] xl:pb-[250px]"
+          marqueeCss="relative top-[-70px] z-10"
+          titleCss="xs:text-[25px] text-[35px] xl:text-[45px] 2xl:text-[50px]"
+        />
+      </div>
+      <div className="py-[80px]">
+        <Awards />
+      </div>
+
+      <div className="py-[80px]">
+        <WhyChoose
+          subTitle="Why Choose Us"
+          title="Because We HelpYour Business Get <br class='lg:block hidden'/> Bigger Every Passing Day!"
+          desc="We know, and you know too, that business survival is so challenging in the internet-focused world. And when 90% of U.S. firms use social media, it becomes even more challenging to survive and keep moving. However, with a sound digital marketing agency that doesn't use cookie-cutter marketing, brands can enjoy impressive growth and sales. But the question arises, where to find such an authentic marketing agency?"
+          data={chooseData}
+        />
+      </div>
+    </main>
+  );
 }
--- src/components/whyChoose.jsx
+++ src/components/whyChoose.jsx
@@ -1,24 +1,30 @@
+import Image from "next/image";
+
 export default function whyChoose({subTitle, title, desc, bgClass, classes = " ", gridsClasses = " ", cta = "", data, theme = "light"}) {
   return (
     <section>
-      <div >
+      <div>
         <div className="container">
-          <div className="text-center mb-12">
+          <div className="text-center mb-16">
             {subTitle && (
-              <span className="text-[20px] lg:text-[25px] bg-[#F8BF19] w-max mx-auto rounded-full py-3 px-[2.5rem] block leading-tight font-normal uppercase text-black mb-3 shadow-sm">
+              <span className="text-[20px] lg:text-[25px] bg-[#F8BF19] w-max mx-auto rounded-full py-3 px-[2.5rem] block leading-tight font-normal uppercase text-black mb-6 shadow-sm">
                 {subTitle}
               </span>
             )}
             {title && (
-              <h3 className={`lg:text-[50px] text-[40px] font-medium text-[#202124] mx-auto ${theme == "light" ? "text-[#202124]" : "text-white"} mb-2`} dangerouslySetInnerHTML={{__html: title}} />
+              <h3 className={`lg:text-[50px] md:text-[40px] text-[25px] font-medium text-[#202124] mx-auto ${theme == "light" ? "text-[#202124]" : "text-white"} mb-2`} dangerouslySetInnerHTML={{__html: title}} />
             )}
-            {desc && <p className={`text-[14px] lg:text-[16px] mx-auto block leading-[26px] ${theme == "light" ? "text-[#202124]" : "text-white"}`} dangerouslySetInnerHTML={{__html: desc}} />}
+            {desc && <p className={`text-[14px] lg:text-[16px] mx-auto block leading-[26px] font-light ${theme == "light" ? "text-[#202124]" : "text-white"}`} dangerouslySetInnerHTML={{__html: desc}} />}
           </div>
 
-          <div className="grid grid-cols-3 gap-3 even:border-[1px] even:border-solid even:shadow-lg">
-                <div>1</div>
-                <div>2</div>
-                <div>3</div>
+          <div className="grid md:grid-cols-3 grid-cols-1 items-center gap-3 ">
+            {data?.map(([image, title, desc], i) => (
+              <div key={i} className="flex flex-col justify-center gap-3 lg:min-h-[450px] min-h-[400px] even:border-[1px] even:border-solid  even:shadow-lg lg:px-[25px] px-[15px] lg:py-[40px] py-[30px] even:rounded-[30px]">
+                <Image src={image} alt="Brandmint" className="w-[70px] h-[70px] mb-[20px]" width={50} height={50} />
+                <h3 className="xl:text-[30px] md:text-[20px] text-[25px] font-semibold border-b-[4px] border-b-[#EBEBEB] pb-5 mb-5" dangerouslySetInnerHTML={{__html: title}} />
+                <p className="xl:text-[16px] text-[14px] font-light" dangerouslySetInnerHTML={{__html: desc}} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
