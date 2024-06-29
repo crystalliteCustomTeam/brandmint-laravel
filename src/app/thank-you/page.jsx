d6ceda7c39e307623c106053381a0fe36659f34d
devsahal-crystallite <143542001+devsahal-crystallite@users.noreply.github.com>
2024-06-29 05:16:52 +0500

    new content and section added

---
+++ src/app/thank-you/page.jsx
@@ -0,0 +1,19 @@
+import {Hero, ServicesTabs, Testimonial } from '@/components'
+import React from 'react'
+
+export default function thankYou() {
+    return (
+      <main>
+        <Hero 
+        title="Thank You"
+        desc="Get your free 30 minutes strategy session with an experienced digital marketer valued at $300USD."
+        />
+        <div className="xs:py-[50px] py-[90px]">
+        <ServicesTabs />
+      </div>
+      <div className="xs:py-[50px] py-[90px]">
+        <Testimonial />
+      </div>
+      </main>
+    )
+  }
