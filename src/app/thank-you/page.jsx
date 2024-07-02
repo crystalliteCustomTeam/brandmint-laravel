eff0326311462167b047f7c7b7bc2ad6ca39a46a
devsahal-crystallite <143542001+devsahal-crystallite@users.noreply.github.com>
2024-07-02 23:52:55 +0500

    content added

--- src/app/thank-you/page.jsx
+++ src/app/thank-you/page.jsx
@@ -7,6 +7,7 @@ export default function thankYou() {
         <Hero
         title="Thank You"
         desc="Get your free 30 minutes strategy session with an experienced digital marketer valued at $300USD."
+        marqueeTheme = "light"
         />
         <div className="xs:py-[50px] py-[90px]">
         <ServicesTabs />
